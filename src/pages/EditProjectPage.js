ade4bbf846f3498da78b625508f8f84763e6603a
jidekickpush <jeandenisdrane@gmail.com>
2022-05-17 12:23:56 +0200

    Fixed update behavior and show information in the form from the DB

--- src/pages/EditProjectPage.js
+++ src/pages/EditProjectPage.js
@@ -12,7 +12,7 @@ function EditProjectPage(props) {
     const projectDetails = props.projects.find( project => project._id === projectId); // get the details of the project that we're trying to edit
 
     const [title, setTitle] = useState(projectDetails.title);
-    const [description, setDescription] = useState();
+    const [description, setDescription] = useState(projectDetails.description);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -24,6 +24,7 @@ function EditProjectPage(props) {
 
         axios.put(`${process.env.REACT_APP_API_URL}/projects/${projectId}`, newDetails)
             .then(response => {
+                props.callBackUpdateProjectsList()
                 navigate("/projects"); // redirect to project list
                 // navigate(`/projects/${response.data._id}`); // redirect to project page
             })
